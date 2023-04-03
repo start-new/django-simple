805fe9641ec9df8230484ecfb5255fcc0ec29525
Thierry Chappuis <tchappui@users.noreply.github.com>
2023-04-03 13:30:36 +0200

    Update tailwind.config.js

--- {{ cookiecutter.project_slug }}/frontend/tailwind.config.js
+++ {{ cookiecutter.project_slug }}/frontend/tailwind.config.js
@@ -1,13 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const Path = require("path");
+const pwd = process.env.PWD;
+
+const projectPaths = [
+  Path.join(pwd, "../{{ cookiecutter.project_slug }}/templates/**/*.html"),
+  Path.join(pwd, "../{{ cookiecutter.project_slug }}/templates/**/*.py"),
+  Path.join(pwd, "./src/**/*.js"),
+];
+
+const pythonPaths = [
+  Path.join(pwd, "../.venv/**/crispy_tailwind/**/*.html"),
+  Path.join(pwd, "../.venv/**/crispy_tailwind/**/*.py"),
+  Path.join(pwd, "../.venv/**/crispy_tailwind/**/*.js"),
+];
+
+const contentPaths = [...projectPaths, ...pythonPaths];
+
+console.log(`tailwindcss will scan ${contentPaths}`);
+
 module.exports = {
-  content: [
-    "../{{ cookiecutter.project_slug }}/templates/**/*.html",
-    "../{{ cookiecutter.project_slug }}/templates/**/*.py",
-    "./src/**/*.js",
-    "../.venv/**/crispy_tailwind/**/*.html",
-    "../.venv/**/crispy_tailwind/**/*.py",
-    "../.venv/**/crispy_tailwind/**/*.js"
-  ],
+  content: contentPaths
   theme: {
     extend: {},
   },
